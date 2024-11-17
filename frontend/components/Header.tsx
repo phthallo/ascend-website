50d45a0198f51d8c1e18f01841553392245d6c51
Louisa Mulei <louisakavindu@gmail.com>
2024-11-17 08:39:54 -0800

    stellar name in titile

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -3,7 +3,7 @@ const Header = () => {
     <div className="bg-[#111f45] h-full w-full py-10 font-bettervcr">
       <div className=" grid grid-cols-8 divide-x-2 divide-[#de8af5] ">
         <h1 className="col-span-2 text-[#de8af5] text-3xl text-center pr-14 pl-10 leading-none">
-          <a href="/">generic name</a>
+          <a href="/">stellar blueprint</a>
         </h1>
         <a className="align-middle items-center col-span-2 text-[#de8af5] text-2xl text-center leading-tight" href="./allitems">
           all materials
